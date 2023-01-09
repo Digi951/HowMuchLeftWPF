a0118fb0593faa86a08cc6c1be4ed8962bf6f7e7
Digi951 <ch.dieckmann@outlook.de>
2023-01-09 16:41:27 +0100

    Fix right location

---
+++ HowMuchLeft.WPF/MVVM/Views/MainView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
+
+namespace HowMuchLeft.WPF.MVVM.Views;
+/// <summary>
+/// Interaktionslogik für MainView.xaml
+/// </summary>
+public partial class MainView : Window
+{  
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr SendMessage(IntPtr hWnd, Int32 wMsg, Int32 wParam, IntPtr lParam);
+
+    private void btnClose_Click(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+
+    private void btnMaximize_Click(object sender, RoutedEventArgs e)
+    {
+        this.WindowState = this.WindowState == WindowState.Normal
+                                            ? WindowState.Maximized
+                                            : WindowState.Normal;
+    }
+
+    private void btnMinimize_Click(object sender, RoutedEventArgs e)
+    {
+        this.WindowState = WindowState.Minimized;
+    }
+
+    private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        WindowInteropHelper helper = new(this);
+        SendMessage(helper.Handle, 161, 2, IntPtr.Zero);
+    }
+
+    private void pnlControlBar_MouseEnter(object sender, MouseEventArgs e)
+    {
+        this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+    }
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
